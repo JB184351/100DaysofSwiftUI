6fabe60e251c6d6bc5ae1331d2449cba63c4b276
JB184351 <justinbengtson@icloud.com>
2023-04-26 16:43:09 -0600

    Finished Day 2 of the 100DaysofSwiftUI

---
+++ Day 2.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Cocoa
+import Foundation
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+//print(number.isMuliple(of: 3))
+
+let goodDogs = true
+var gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+//print(gameOver)
+gameOver.toggle()
+//print(gameOver)
+
+let people = "Haters"
+let action = "hate"
+
+let lyric = people + " gonna " + action
+
+let luggageCode = "12345"
+
+let name = "Taylor"
+let age = 26
+
+let greeting = "Hello my name is \(name) and I'm \(age) years old"
+//print(greeting)
+
+let celsiusTemp = 32.0
+let fahrenheit = celsiusTemp * (9/5) + 32
+print(fahrenheit)
+
