3d559331b3f79d1c54ee28f160a5e29d984fa68f
JB184351 <justinbengtson@icloud.com>
2023-04-25 18:36:07 -0600

    Day 1 of 100DaysofSwiftUI finished!

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,74 @@
+import Cocoa
+
+// Day 1 of the 100DaysOfSwiftUI challenge
+
+var name = "Justin"
+name = "Brett"
+name = "Ted"
+
+let character = "Charlie"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "42"
+
+let actor = "Adam Sandler"
+let filename = "paris.jpg"
+let basketball = "This is a basketball 🏀"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple Engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(basketball.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+let score = 10
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+
+print(counter)
+
+let decimalNumber = 0.1 + 0.2
+print(decimalNumber)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+
+let int1 = 3
+
+var actorName = "Nicolas Cage"
+actorName = "Mando"
+
+var rating = 5.0
+rating += 2
+
+
+
