68b92ce3030e82e8c3d1f59365057463dbaf7cb9
JB184351 <justinbengtson@icloud.com>
2023-05-24 12:20:28 -0600

    day 14

--- day 14.playground/Contents.swift
+++ day 14.playground/Contents.swift
@@ -1,3 +1,65 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+let peachOpposite = opposites["Peach"]
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number: Int? = nil
+
+if let number = number {
+    print(square(number: number))
+}
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+    
+    print("\(number) x \(number) is \(number * number)")
+}
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
+
+// Checkpoint 9
+
+func getARandomNumber(nums: [Int]?) -> Int {
+    return nums?.randomElement() ?? Array(1...100).randomElement()!
+}
