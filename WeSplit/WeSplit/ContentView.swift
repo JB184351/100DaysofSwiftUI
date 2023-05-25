ece5859c983a62090a04a9a74a0b677cbb7a55f1
JB184351 <justinbengtson@icloud.com>
2023-05-25 16:11:02 -0600

    day 16

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Justin on 5/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) { student in
+                        Text(student)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Justin on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
