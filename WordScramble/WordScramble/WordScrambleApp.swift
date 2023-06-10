772ac66a649fad5b36deb9539248bff61d1e1a85
JB184351 <justinbengtson@icloud.com>
2023-06-10 11:39:13 -0600

    Created Wordscramble game as part of 100DaysofSwiftUI days 28-30

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Justin on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
