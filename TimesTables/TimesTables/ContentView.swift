3534655378c72f76f936f45fee42a5bcb3375a4e
JB184351 <justinbengtson@icloud.com>
2023-06-16 17:38:46 -0600

    Able to display the List of problems correctly now

--- TimesTables/TimesTables/ContentView.swift
+++ TimesTables/TimesTables/ContentView.swift
@@ -14,13 +14,14 @@ enum QuestionDifficulty: String, CaseIterable, Equatable, Hashable {
 }
 
 struct ContentView: View {
-    @State private var timesTablesSelection = 0
+    @State private var timesTablesSelection = 2
     @State private var questionDifficulty: QuestionDifficulty = .easy
     @State private var numberOfQuestions = 5
     @State private var numberOfQuestionsCorrect = 0
-    @State private var answer = 0
+    @State private var answer = ""
     
     let numberOfQuestionsToAnswer = [5, 10, 20]
+    let timesTables = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     
     @State private var questions: [[Int]] = [[0, 0]]
     
@@ -29,12 +30,12 @@ struct ContentView: View {
             Form {
                 Section {
                     Picker("", selection: $timesTablesSelection) {
-                        ForEach(2..<13) {
+                        ForEach(timesTables, id:\.self) {
                             Text("\($0)")
                         }
                     }
                     .pickerStyle(.segmented)
-                    .onChange(of: timesTablesSelection) { timesTablesSelection in
+                    .onChange(of: timesTablesSelection) { newValue in
                         generateQuestions(difficulty: questionDifficulty, numberOfQuestions: numberOfQuestions, multiple: timesTablesSelection)
                     }
                 } header: {
@@ -73,7 +74,12 @@ struct ContentView: View {
                 
                 List {
                     ForEach(questions, id: \.self) {
-                        Text("\($0[0]) x \($0[1])")
+                        
+                        TextField("\($0[0]) x \($0[1])", text: $answer)
+                            .keyboardType(.numberPad)
+                    }
+                    .refreshable {
+                        generateQuestions(difficulty: questionDifficulty, numberOfQuestions: numberOfQuestions, multiple: timesTablesSelection)
                     }
                 }
             }
@@ -83,16 +89,18 @@ struct ContentView: View {
     
     func generateQuestions(difficulty: QuestionDifficulty, numberOfQuestions: Int, multiple: Int) {
         var easyDifficulty = Array([[2, 2], [3, 2], [2, 3], [4, 2], [2, 4], [3, 3], [4, 3], [4, 4], [4, 5], [3, 5], [2, 5], [5, 5], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [2, 12], [2, 11], [2, 10], [2, 9], [2, 8], [2, 7], [5, 3]].filter { subArray in
-            subArray.contains(2)
+            subArray.contains(multiple)
         }.shuffled())
         
         easyDifficulty = Array(easyDifficulty.prefix(numberOfQuestions))
-                                   
-        let mediumDifficulty = Array([[5, 6], [5, 7], [5, 8], [5, 9], [5, 10], [5, 11], [5, 12], [6, 6], [6, 7], [6, 8], [6, 9], [6, 10], [6, 11], [6, 12], [7, 7], [7, 8], [7, 9], [7, 10], [7, 11], [7, 12], [8, 8]].shuffled().prefix(numberOfQuestions))
         
-        let hardDifficulty = Array([[12, 2], [3, 11], [12, 11], [4, 12], [12, 5], [11, 4], [9, 8], [12, 9], [9, 10], [10, 12], [11, 10]].shuffled().prefix(numberOfQuestions))
+        var mediumDifficulty = Array([[5, 6], [5, 7], [5, 8], [5, 9], [5, 10], [5, 11], [5, 12], [6, 6], [6, 7], [6, 8], [6, 9], [6, 10], [6, 11], [6, 12], [7, 7], [7, 8], [7, 9], [7, 10], [7, 11], [7, 12], [8, 8]].shuffled().prefix(numberOfQuestions))
         
-        print(easyDifficulty)
+        mediumDifficulty = Array(easyDifficulty.prefix(numberOfQuestions))
+        
+        var hardDifficulty = Array([[12, 2], [3, 11], [12, 11], [4, 12], [12, 5], [11, 4], [9, 8], [12, 9], [9, 10], [10, 12], [11, 10]].shuffled().prefix(numberOfQuestions))
+        
+        hardDifficulty = Array(easyDifficulty.prefix(numberOfQuestions))
         
         switch difficulty {
         case .easy:
