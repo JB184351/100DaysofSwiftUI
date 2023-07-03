47dc4fc813de4f3715f78c78e956d3e0473d23c4
JB184351 <justinbengtson@icloud.com>
2023-07-03 11:44:39 -0600

    Codable conformance for @Published Properties.

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Justin on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
