3a5aa4c8237dd1ce5d50cfb691a15e5871a60eac
JB184351 <justinbengtson@icloud.com>
2023-07-03 12:18:30 -0600

    Loading an async image

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,47 +7,22 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
 
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async {
-        guard let url =  URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            // more to come
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
+        .frame(width: 200, height: 200)
     }
 }
 
