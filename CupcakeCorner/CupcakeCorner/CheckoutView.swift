eee69e2385baff1e2eab1829a52ed0a5f250e54d
JB184351 <justinbengtson@icloud.com>
2023-07-05 12:24:25 -0600

    Finished PlaceOrder method

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMesssage = ""
+    @State private var showingConfirmationMessage = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -26,16 +29,48 @@ struct CheckoutView: View {
                     .font(.title)
                 
                 Button("Place Order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
         .navigationBarTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmationMessage) {
+            Button("OK") {}
+        } message: {
+            Text(confirmationMesssage)
+        }
     }
     
-    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        guard let url = URL(string: "https://reqres.in/api/cupcakes") else {
+            print("Not a valid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMesssage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmationMessage = true
+            
+        } catch {
+            print("Order failed to send")
+        }
+    }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
