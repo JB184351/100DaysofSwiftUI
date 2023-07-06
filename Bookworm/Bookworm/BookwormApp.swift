302177889e37ec1035359872ee33ffd9b83b5a30
JB184351 <justinbengtson@icloud.com>
2023-07-06 11:02:41 -0600

    Intro to Binding

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Justin on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
