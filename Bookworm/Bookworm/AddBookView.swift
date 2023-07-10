a92776cc13d2a4c420c8a16ece0f2939baedd7c2
JB184351 <justinbengtson@icloud.com>
2023-07-10 11:18:00 -0600

    Challenge 1 for Day 56

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -14,7 +14,7 @@ struct AddBookView: View {
     @State private var title = ""
     @State private var author = ""
     @State private var rating = 3
-    @State private var genre = ""
+    @State private var genre = "Fantasy"
     @State private var review = ""
     
     let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
@@ -55,6 +55,8 @@ struct AddBookView: View {
                         
                         dismiss()
                     }
+                    // Challenge 1 Day 56
+                    .disabled(title.isEmpty || author.isEmpty || genre.isEmpty)
                 }
             }
             .navigationTitle("Add Book")
