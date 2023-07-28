0ae2e6cbdc5c926b513e8aa7bdfc0e2440d71c32
JB184351 <justinbengtson@icloud.com>
2023-07-28 10:05:15 -0600

    Added accessibility to Word Scramble

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -29,6 +29,8 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel("\(word), \(word.count) letters")
                         
                     }
                 }
