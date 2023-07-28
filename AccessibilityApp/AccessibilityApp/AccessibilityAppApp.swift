12ce2fe7f479a93ade0eff998c2bf6c88a63a4ce
JB184351 <justinbengtson@icloud.com>
2023-07-28 10:00:41 -0600

    Testing Accessibility things

---
+++ AccessibilityApp/AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Justin on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
