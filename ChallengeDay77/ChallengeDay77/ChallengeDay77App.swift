283d1416ad0eef5d360cee4456b5b28e09adcf30
JB184351 <justinbengtson@icloud.com>
2023-07-31 11:02:52 -0600

    Started Day 77 challenge

---
+++ ChallengeDay77/ChallengeDay77/ChallengeDay77App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay77App.swift
+//  ChallengeDay77
+//
+//  Created by Justin on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeDay77/ChallengeDay77/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  ChallengeDay77
+//
+//  Created by Justin on 7/31/23.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
