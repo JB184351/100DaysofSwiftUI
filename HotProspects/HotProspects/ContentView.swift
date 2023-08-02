879a136c5ca392e6b963ffc15b10143e7d2f9359
JB184351 <justinbengtson@icloud.com>
2023-08-02 11:24:18 -0600

    Using Result Type

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Justin on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var output = ""
+    
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Download error: \(error.localizedDescription)"
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
