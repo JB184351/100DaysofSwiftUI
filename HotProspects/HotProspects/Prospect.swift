c9e9b686d28323448aa0f0a6cd058b34af21ff16
JB184351 <justinbengtson@icloud.com>
2023-08-11 13:50:35 -0600

    Now using JSON and documents directory for saving and loading user data

--- HotProspects/HotProspects/Prospect.swift
+++ HotProspects/HotProspects/Prospect.swift
@@ -16,24 +16,28 @@ class Prospect: Identifiable, Codable {
 
 @MainActor class Prospects: ObservableObject {
     @Published private(set) var people: [Prospect]
-    let saveKey = "SavedData"
+    let savePath = FileManager.documentsDirectory.appendingPathComponent("Prospects")
     
+    // Challenge 2: Use JSON and documents directory for saving
+    // nad loading data
     init() {
-        if let data = UserDefaults.standard.data(forKey: saveKey) {
-            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
-                people = decoded
-                return
-            }
+        do {
+            let data = try Data(contentsOf: savePath)
+            people = try JSONDecoder().decode([Prospect].self, from: data)
+        } catch {
+            people = []
         }
-        
-        people = []
     }
     
     private func save() {
-        if let encoded = try? JSONEncoder().encode(people) {
-            UserDefaults.standard.set(encoded, forKey: saveKey)
+        do {
+            let data = try JSONEncoder().encode(people)
+            try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data.")
         }
     }
+
     
     func add(_ prospect: Prospect) {
         people.append(prospect)
