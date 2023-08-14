26d4cbe201200fe49d972e5daaaecde435b4d2c9
JB184351 <justinbengtson@icloud.com>
2023-08-14 12:12:38 -0600

    Trying out haptics

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Justin on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
