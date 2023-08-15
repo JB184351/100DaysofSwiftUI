aeb43da1cfbe1f8732e07337931cc041c4bd7211
JB184351 <justinbengtson@icloud.com>
2023-08-15 10:08:11 -0600

    Made a simple card view

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Justin on 8/15/23.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
