d43aab90119acb53db6c87cd8897d8e0c59206ee
JB184351 <justinbengtson@icloud.com>
2023-08-17 12:21:00 -0600

    Challenge 2 completed.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -21,7 +21,10 @@ struct ContentView: View {
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             // Challenge 1, I think? It does disappear??
-                            .opacity(geo.frame(in: .global).minY <= 150 ? 0 : 1)
+//                            .opacity(geo.frame(in: .global).minY <= 150 ? 0 : 1)
+                        // Challenge 2
+                            .scaleEffect(geo.frame(in: .global).minY <= 150 ? 0.5 : 1)
+                            .scaleEffect(geo.frame(in: .global).minY >= 700 ? 2.0 : 1)
                     }
                     .frame(height: 40)
                 }
